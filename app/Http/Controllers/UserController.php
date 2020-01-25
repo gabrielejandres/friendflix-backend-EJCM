9897718014f01feb3a7ae2d74b3b31e708d2dcc6
gabrielejandres <gabrielejandres@gmail.com>
2020-01-25 11:38:47 -0300

    Versão aula

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    //MÉTODOS CRUD
+
+
+	//create
+    public function createUser(Request $request){
+
+    	$user = new User;
+
+    	$user->name = $request->name;
+    	$user->email = $request->email;
+    	$user->password = $request->password;
+    	$user->save(); //salva os dados
+
+    	return response()->json([$user]); //retorna o que inserimos
+    }
+
+    //read
+    public function listUser(){
+
+    	$user = User::all(); //User é a model sendo utilizada, o all indica todas as instâncias 
+    	return response()->json($user);
+    }
+
+    public function showUser($id){
+
+    	$user = User::findOrFail($id);
+    	return response()->json([$user]);
+    }
+
+    //update
+    public function updateUser(Request $request, $id){//request é tudo o que quer atualizar
+
+    	$user = User::find($id);
+
+    	if($user){ //se achar o usuário faz as atualizações
+
+    		//os ifs verificam qual foi o parâmetro passado para ser atualizado
+    		if($request->name){
+    			$user->name = $request->name;
+    		}
+    		else if($request->email){
+    			$user->email = $request_email;
+    		}
+    		else if($request->password){
+    			$user->password = $request->password;
+    		}
+    		else{
+    			return response()->json(['Insira o parâmetro a ser atualizado']);
+    		}
+    		$user->save();
+    		return response()->json([$user]);
+    	}
+    	else{
+    		return response()->json(['Este usuário não existe']);
+    	}
+    }
+
+    //delete
+    public function deleteUser($id){
+
+    	User::destroy($id);
+    	return response()->json(['Usuário deletado']); 
+
+    }
+}
---
+++ database/migrations/2020_01_24_125058_create_series_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() //instruções para a tabela no banco de dados, qualquer coluna insere na tabela
+    {
+        Schema::create('series', function (Blueprint $table) { //migration é a entidade da modelagem 
+            //TRADUÇÃO DA MODELAGEM - CRIAÃO DAS TABELAS
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->longText('sinopse');
+            $table->integer('ano')->unsigned();
+            $table->integer('avaliacao_geral')->unsigned();
+            $table->boolean('premiada');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+        });
+
+        //Foreign Keys
+        Schema::table('series', function (Blueprint $table) { 
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('series');
+    }
+}
